cfe9ae757c5a4dd81792daa61f6f3cf4b9af0861
Ridaniel <ri.da.igle.es@gmail.com>
2019-11-14 10:16:25 -0500

    Se muestra uso de memoria por consola

--- Interfaz/Algorithms.py
+++ Interfaz/Algorithms.py
@@ -1,3 +1,4 @@
+from memory_profiler import profile
 class Algorithms():
     
     def calVolumen(self, arr):
@@ -88,7 +89,7 @@ class Algorithms():
         
             self.quicksort(arr, ini, pi-1, pos)
             self.quicksort(arr, pi+1, fin, pos)
-    
+    @profile
     def NFDH(self, arrItem, largoBin, anchBin, altoBin):
         n = len(arrItem)
         self.quicksort(arrItem, 0, n-1, 7)
@@ -147,7 +148,7 @@ class Algorithms():
                         
         
         return arrItem, bins
-    
+    @profile
     def Algorithm(self, rectangles, anchoC, altoC, largoC):
     
         n = len(rectangles)
